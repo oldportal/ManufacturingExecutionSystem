a1c2944dff567f94659adf1cb254a4b0683422c9
Dmitry Ognyannikov <dmitry.ogn@gmail.com>
2019-01-27 14:42:09 -0300

    ExternalTaskService example

--- src/main/java/com/oldportal/mes/webbeans/BpmnProcessesCamunda.java
+++ src/main/java/com/oldportal/mes/webbeans/BpmnProcessesCamunda.java
@@ -5,10 +5,14 @@
  */
 package com.oldportal.mes.webbeans;
 
+import java.util.List;
+import org.camunda.bpm.engine.ExternalTaskService;
 import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.RepositoryService;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.externaltask.LockedExternalTask;
+import org.camunda.bpm.engine.variable.VariableMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +29,9 @@ public class BpmnProcessesCamunda {
 
     @Autowired
     private TaskService taskService;
+    
+    @Autowired
+    private ExternalTaskService externalTaskService;
 
     @Autowired
     private RepositoryService repositoryService;
@@ -32,5 +39,39 @@ public class BpmnProcessesCamunda {
     @Autowired
     private HistoryService historyService;
     
+    
+    public void fetchAndExceuteTask(String externalWorkerId) {
+        try {
+            // 10 tasks
+            List<LockedExternalTask> tasks = externalTaskService.fetchAndLock(10, externalWorkerId)
+            .topic("AddressValidation", 60L * 1000L).variables("address")
+            .execute();
+            for (LockedExternalTask task : tasks) {
+                try {
+                    String topic = task.getTopicName();
+                    //MyAddressClass address = (MyAddressClass) task.getVariables().get("address");
+
+
+                    // if the work is successful, mark the task as completed
+                    boolean success = true;
+                    if(success) {
+                        externalTaskService.complete(task.getId(), externalWorkerId);
+                    }
+                    else {
+                      // if the work was not successful, mark it as failed
+                      externalTaskService.handleFailure(
+                        task.getId(),
+                        externalWorkerId,
+                        "Address could not be validated: Address database not reachable",
+                        1, 10L * 60L * 1000L);
+                    }
+                } catch(Exception e) {
+                    //... handle exception
+                }
+            }
+        } finally {
+            externalTaskService.unlock(externalWorkerId);
+        }
+    }
     
 }
